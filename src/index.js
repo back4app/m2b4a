40126d1cec1858d9b7e5064142b855cecdb8f045
Ricardo Paiva <ricardo@ricardopaiva.com>
2019-05-09 11:09:17 -0300

    Print stacktrace on errors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ module.exports = async function run () {
 
   await prompt().catch(err => {
     console.log('\n\nOoops... Something got wrong!\nPlease open a ticket at: https://www.back4app.com\n\n'.red.bold)
-    console.error(err)
+    console.error(err.message || err)
+    console.error(err.stack)
   })
 
 }
