9ee2a8edfc2e7b0d778c666efc3ec4e54e188dfb
Ricardo Paiva <ricardo@ricardopaiva.com>
2019-05-10 16:32:39 -0300

    Print version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const prompt = require('./prompt')
 const colors = require('colors')
 
+const pjson = require('../package.json');
+console.log(`\nBack4App Migration Tool Version ${pjson.version.yellow}\n`.blue.bold);
+
 colors.setTheme({
   custom: ['grey']
 })
